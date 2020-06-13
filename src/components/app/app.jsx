801b3b8287391bff0a071644cc8b1c0886696559
Игорь Насибов <nasibov.iv@yandex.ru>
2020-06-13 14:11:11 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import Main from "../main/main.jsx";
+import PropTypes from "prop-types";
 
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {filmName, filmGenre, filmDate, filmImage} = props;
+  const {filmName, filmGenre, filmDate, filmImage, films} = props;
 
   return (
-    <Main filmName={filmName} filmGenre={filmGenre} filmDate={filmDate} filmImage={filmImage}/>
+    <Main filmName={filmName} filmGenre={filmGenre} filmDate={filmDate} filmImage={filmImage} films={films}/>
   );
+};
+
+App.propTypes = {
+  filmName: PropTypes.string.isRequired,
+  filmGenre: PropTypes.string.isRequired,
+  filmDate: PropTypes.number.isRequired,
+  filmImage: PropTypes.string.isRequired,
+  films: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 
