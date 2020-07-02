a52c69b50886e7fa89f5b36f6bc98b0e2967397b
Игорь Насибов <nasibov.iv@yandex.ru>
2020-07-02 21:10:50 +0300

    тест

--- src/components/show-more-button/show-more-button.jsx
+++ src/components/show-more-button/show-more-button.jsx
@@ -5,9 +5,11 @@ import PropTypes from "prop-types";
 const ShowMoreButton = (props) => {
   const {onClick} = props;
   return (
+
     <div className="catalog__more" onClick={onClick}>
       <button className="catalog__button" type="button">Show more</button>
     </div>
+
   );
 };
 
